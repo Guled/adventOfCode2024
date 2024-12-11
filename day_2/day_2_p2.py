8f7fb0c8b839b6e667229aff5614664073198261
guled <contact@guledabdilahi.com>
2024-12-11 03:59:14 -0500

    unknown answer p2

--- day_2/day_2_p2.py
+++ day_2/day_2_p2.py
@@ -1,6 +1,6 @@
 
-# Advent of Code Day 2
-# Answer: 526
+## TBD need to fix the logic for this one.
+## Answer not 558, 558 is too low.
 multiline_string = """
 7 6 4 2 1
 1 2 7 8 9
@@ -30,6 +30,7 @@ def validate_list(lst):
         line = list(filter(lambda x: x != '', line))
         state = "unknown"
         safe = True
+        freebie = True
 
         for report_index in range(len(line)):
             if len(line) == 0 or len(line) == 1 or report_index == len(line) - 1:
@@ -43,22 +44,33 @@ def validate_list(lst):
                         state = "decreasing"
                     if abs(difference) < 1 or abs(difference) > 3:
                             safe = False
+
                 else:
                     difference = int(line[report_index]) - int(line[report_index + 1])
                     if difference > 0 and state == "decreasing":
-                        safe = False
+                        if freebie:
+                            freebie = False
+                        else:   
+                            safe = False
                     elif difference < 0 and state == "increasing":
-                        safe = False
+                        if freebie:
+                            freebie = False
+                        else:
+                            safe = False
                     if abs(difference) < 1 or abs(difference) > 3:
-                        safe = False
+                        if freebie:
+                            freebie = False
+                        else:
+                            safe = False
                         
         if safe:
             count_safe_reports += 1
 
     return count_safe_reports
 
+
 if __name__ == "__main__":
     # print(validate_list(parse_input_text(multiline_string)))
-    print(f'Part 1 Answer: {validate_list(parse_input_file('day_2/input.txt'))}')
+    print(f'Part 2 Answer: {validate_list(parse_input_file('day_2/input.txt'))}')
 
 
